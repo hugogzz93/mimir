54699213bf384b504bfe9ca550a56d86a745ec83
Hugo Gonzalez <pinelo93@gmail.com>
2017-03-04 22:17:11 -0600

    Adds event creation to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   	@state = Money.state
     @banner = Banner.last
     @tasks =  Task.tasks_for Time.zone.now, "day"
-    @objects = [Money.new, Activity.new, Weight.new, Task.new, Banner.new]
+    @objects = [Money.new, Activity.new, Weight.new, Task.new, Banner.new, Event.new]
     @object_data = @objects.map { |x| x.class.chart_digest({},Time.zone.now, "year") }
     gon.objects = @objects.map { |x| x.class.name.downcase }
     gon.object_data = @object_data
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,18 @@
 class Event < ApplicationRecord
+	include Valued
 	belongs_to :user
 	class << self
 		def events_for(scope, date, range)
 			by_date(date, range).where scope
 		end
+
+		def label
+			"Duration"
+		end
+	end
+
+	def value
+		( self.end_time - self.start_time ) / 60
 	end
 
 	def day
